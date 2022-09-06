80d02a666b3db4ab0879d14de010db32dee71bf7
Gyau-1 <91359722+Gyau-1@users.noreply.github.com>
2022-09-06 03:09:16 +0000

    Add files via upload

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace SHOPRITE_GH
+﻿namespace InventorySystem1._0
 {
     partial class Form1
     {
@@ -29,185 +29,137 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textName = new System.Windows.Forms.TextBox();
-            this.textPass = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.checkBoxPass = new System.Windows.Forms.CheckBox();
-            this.lblClear = new System.Windows.Forms.Label();
-            this.btnLogin = new System.Windows.Forms.Button();
-            this.panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.ts_stocks = new System.Windows.Forms.ToolStripButton();
+            this.ts_StockOut = new System.Windows.Forms.ToolStripButton();
+            this.ts_Return = new System.Windows.Forms.ToolStripButton();
+            this.ts_ManageUsers = new System.Windows.Forms.ToolStripButton();
+            this.ts_settings = new System.Windows.Forms.ToolStripButton();
+            this.ts_Report = new System.Windows.Forms.ToolStripButton();
+            this.ts_Login = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // panel1
+            // toolStrip1
             // 
-            this.panel1.BackColor = System.Drawing.Color.Black;
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.pictureBox1);
-            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(439, 100);
-            this.panel1.TabIndex = 0;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(50, 35);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ts_stocks,
+            this.ts_StockOut,
+            this.ts_Return,
+            this.ts_ManageUsers,
+            this.ts_settings,
+            this.ts_Report,
+            this.ts_Login});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(107, 556);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
             // 
-            // label4
+            // ts_stocks
             // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label4.Location = new System.Drawing.Point(195, 58);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(116, 32);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "GH LTD";
+            this.ts_stocks.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ts_stocks.Image = ((System.Drawing.Image)(resources.GetObject("ts_stocks.Image")));
+            this.ts_stocks.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ts_stocks.Name = "ts_stocks";
+            this.ts_stocks.Size = new System.Drawing.Size(104, 59);
+            this.ts_stocks.Text = "Stocks Master";
+            this.ts_stocks.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ts_stocks.Click += new System.EventHandler(this.ts_stocks_Click);
             // 
-            // label3
+            // ts_StockOut
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaption;
-            this.label3.Location = new System.Drawing.Point(174, 26);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(157, 32);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "SHOPRITE";
+            this.ts_StockOut.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ts_StockOut.Image = ((System.Drawing.Image)(resources.GetObject("ts_StockOut.Image")));
+            this.ts_StockOut.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ts_StockOut.Name = "ts_StockOut";
+            this.ts_StockOut.Size = new System.Drawing.Size(104, 59);
+            this.ts_StockOut.Text = "Stock-out";
+            this.ts_StockOut.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ts_StockOut.Click += new System.EventHandler(this.ts_StockOut_Click);
             // 
-            // pictureBox1
+            // ts_Return
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(9, 14);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(124, 68);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 6;
-            this.pictureBox1.TabStop = false;
+            this.ts_Return.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ts_Return.Image = ((System.Drawing.Image)(resources.GetObject("ts_Return.Image")));
+            this.ts_Return.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ts_Return.Name = "ts_Return";
+            this.ts_Return.Size = new System.Drawing.Size(104, 59);
+            this.ts_Return.Text = "Stock Return";
+            this.ts_Return.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ts_Return.Click += new System.EventHandler(this.ts_Return_Click);
             // 
-            // textName
+            // ts_ManageUsers
             // 
-            this.textName.Location = new System.Drawing.Point(16, 309);
-            this.textName.Name = "textName";
-            this.textName.Size = new System.Drawing.Size(247, 26);
-            this.textName.TabIndex = 1;
+            this.ts_ManageUsers.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ts_ManageUsers.Image = ((System.Drawing.Image)(resources.GetObject("ts_ManageUsers.Image")));
+            this.ts_ManageUsers.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ts_ManageUsers.Name = "ts_ManageUsers";
+            this.ts_ManageUsers.Size = new System.Drawing.Size(104, 59);
+            this.ts_ManageUsers.Text = "Manage Users";
+            this.ts_ManageUsers.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ts_ManageUsers.Click += new System.EventHandler(this.ts_ManageUsers_Click);
             // 
-            // textPass
+            // ts_settings
             // 
-            this.textPass.Location = new System.Drawing.Point(18, 394);
-            this.textPass.Name = "textPass";
-            this.textPass.Size = new System.Drawing.Size(247, 26);
-            this.textPass.TabIndex = 2;
-            this.textPass.UseSystemPasswordChar = true;
+            this.ts_settings.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ts_settings.Image = ((System.Drawing.Image)(resources.GetObject("ts_settings.Image")));
+            this.ts_settings.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ts_settings.Name = "ts_settings";
+            this.ts_settings.Size = new System.Drawing.Size(104, 59);
+            this.ts_settings.Text = "Settings";
+            this.ts_settings.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ts_settings.Click += new System.EventHandler(this.ts_settings_Click);
             // 
-            // label1
+            // ts_Report
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Monotype Corsiva", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(13, 279);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(147, 26);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "USER NAME:";
+            this.ts_Report.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ts_Report.Image = ((System.Drawing.Image)(resources.GetObject("ts_Report.Image")));
+            this.ts_Report.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ts_Report.Name = "ts_Report";
+            this.ts_Report.Size = new System.Drawing.Size(104, 59);
+            this.ts_Report.Text = "Report";
+            this.ts_Report.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ts_Report.Click += new System.EventHandler(this.ts_Report_Click);
             // 
-            // label2
+            // ts_Login
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(20, 364);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(134, 26);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "PASSWORD:";
+            this.ts_Login.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ts_Login.Image = global::InventorySystem1._0.Properties.Resources.login;
+            this.ts_Login.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.ts_Login.Name = "ts_Login";
+            this.ts_Login.Size = new System.Drawing.Size(104, 59);
+            this.ts_Login.Text = "Login";
+            this.ts_Login.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.ts_Login.Click += new System.EventHandler(this.ts_Login_Click);
             // 
-            // pictureBox2
+            // statusStrip1
             // 
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(150, 106);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(133, 87);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 7;
-            this.pictureBox2.TabStop = false;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 11F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(155, 212);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(123, 26);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Please Login";
-            // 
-            // checkBoxPass
-            // 
-            this.checkBoxPass.AutoSize = true;
-            this.checkBoxPass.Location = new System.Drawing.Point(248, 498);
-            this.checkBoxPass.Name = "checkBoxPass";
-            this.checkBoxPass.Size = new System.Drawing.Size(148, 24);
-            this.checkBoxPass.TabIndex = 9;
-            this.checkBoxPass.Text = "Show Password";
-            this.checkBoxPass.UseVisualStyleBackColor = true;
-            this.checkBoxPass.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // lblClear
-            // 
-            this.lblClear.AutoSize = true;
-            this.lblClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblClear.ForeColor = System.Drawing.Color.Red;
-            this.lblClear.Location = new System.Drawing.Point(29, 498);
-            this.lblClear.Name = "lblClear";
-            this.lblClear.Size = new System.Drawing.Size(64, 26);
-            this.lblClear.TabIndex = 10;
-            this.lblClear.Text = "Clear";
-            this.lblClear.Click += new System.EventHandler(this.label6_Click);
-            // 
-            // btnLogin
-            // 
-            this.btnLogin.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnLogin.Font = new System.Drawing.Font("Sitka Small", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogin.ForeColor = System.Drawing.Color.Teal;
-            this.btnLogin.Location = new System.Drawing.Point(119, 444);
-            this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(225, 38);
-            this.btnLogin.TabIndex = 5;
-            this.btnLogin.Text = "LOGIN";
-            this.btnLogin.UseVisualStyleBackColor = false;
-            this.btnLogin.Click += new System.EventHandler(this.button1_Click);
+            this.statusStrip1.Location = new System.Drawing.Point(107, 534);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(881, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Teal;
-            this.ClientSize = new System.Drawing.Size(439, 653);
-            this.ControlBox = false;
-            this.Controls.Add(this.lblClear);
-            this.Controls.Add(this.checkBoxPass);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.btnLogin);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textPass);
-            this.Controls.Add(this.textName);
-            this.Controls.Add(this.panel1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.ClientSize = new System.Drawing.Size(988, 556);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.toolStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.IsMdiContainer = true;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inventory System Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -215,19 +167,15 @@
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.TextBox textName;
-        private System.Windows.Forms.TextBox textPass;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.CheckBox checkBoxPass;
-        private System.Windows.Forms.Label lblClear;
-        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton ts_stocks;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripButton ts_StockOut;
+        private System.Windows.Forms.ToolStripButton ts_Return;
+        private System.Windows.Forms.ToolStripButton ts_ManageUsers;
+        private System.Windows.Forms.ToolStripButton ts_Report;
+        private System.Windows.Forms.ToolStripButton ts_Login;
+        private System.Windows.Forms.ToolStripButton ts_settings;
     }
 }
 
